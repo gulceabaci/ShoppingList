9319363d7446d2b9383c87dc9519f376217f327e
gulceabaci <gulceabaci_66@hotmail.com>
2025-02-07 20:55:43 +0300

    App.jsx

--- ShoppingList/src/App.jsx
+++ ShoppingList/src/App.jsx
@@ -1,5 +1,9 @@
 import { useState } from "react";
 import { data } from "./data";
+import Header from "./Components/Header";
+import Form from "./Components/Form";
+import List from "./Components/List";
+import Summary from "./Components/Summary";
 
 function App() {
   const [items, setItems] = useState(data);
@@ -29,84 +33,6 @@ function App() {
       <List items={items} onDeleteItem={handleDeleteItem} onUpdateItem={handleUpdateItem}/>
       <Summary items={items} />
    </div>
-  );
-}
-
-function Form({onAddItem, onClearList}) {
-  const [title, setTitle] = useState("");
-  const [quantity, setQuantity] = useState(1);
-  
-
-  function handleFormSubmit(e) {
-    e.preventDefault();
-    if(title) { //submit olayını tetiklemez.
-      const item = {id: Date.now(), title, quantity, completed: false}
-      onAddItem(item);
-      setTitle(''); //kaydedildikten sonra sıfırlanır.
-      setQuantity(1);
-    }
-  }
-
-  return (
-    <form className="form" onSubmit={handleFormSubmit}>
-      <input 
-       value={title} 
-       type="text" 
-       placeholder="Ürün adı giriniz." 
-       onChange={(e) => setTitle(e.target.value)}
-      />  
-      <select value={quantity} onChange={(e) => setQuantity(Number(e.target.value))}>
-        {Array.from({length:10}, (v,i) => i +1).map(num => <option value={num} key={num}>{num}</option>)}
-      </select>
-      <button type="submit">➕ Ekle</button>
-      <button type="button" onClick={onClearList}>🗑️ Temizle</button>
-    </form>
-  );
-}
-
-function List({items, onDeleteItem, onUpdateItem}) {
-  return <>
-    {
-      items.length > 0 ? (
-       <div className="list">
-          <ul>{items.map((i, index) => (<Item item={i} key={index} onDeleteItem={onDeleteItem} onUpdateItem={onUpdateItem} />))}</ul>
-        </div>
-      ) : <div className="list"> Sepette ürün yok</div>
-    }
-   </>;
-}
-
-function Item({item, onDeleteItem, onUpdateItem}) {
-  return(
-    <li>
-      <input 
-        type="checkbox" 
-        checked={item.completed} 
-        onChange={() => onUpdateItem(item.id)}
-      />
-      <span style={item.completed ? {textDecoration:"line-through"} : {} }>{item.quantity} {item.title}</span>
-      <button onClick={() => onDeleteItem(item.id)}>X</button>
-    </li>
-  );
-}
-
-function Summary({items}) {
-  if(items.length === 0) {
-    return(
-      <footer className="summary">Alışeriş Listenizi hazırlamaya başlayabilirsiniz.</footer>
-    );
-  }
-  const itemsCount = items.length;
-  const completedItemsCount = items.filter(item => item.completed). length;
-
-  return(
-    <footer className="summary">
-      {
-        itemsCount === completedItemsCount ? 
-        <p>Alışverişi tamamladınız.</p> : 
-        <p>Alışveriş sepetinizde {itemsCount} üründen {completedItemsCount} tanesi alındı. </p>
-      }
-      </footer>
   );
 }
 
